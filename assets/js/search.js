f8c7ea700500d348546a0c671953a273eaa4e648
Kev-rod43 <kev.rod43@yahoo.com>
2023-12-12 11:32:06 -0500

    added a way to make the stock cards be sortable using uiKit

--- assets/js/search.js
+++ assets/js/search.js
@@ -63,181 +63,182 @@ if (localStorage.getItem("active-tickers")) {
 
 //Displays the stock lookup functionality, and adds event listener to the submit to perform the utility.
 (function () {
-  
-    let formEl = $(
-      "<form id = 'stock-look-up-form' class = 'uk-margin'></form>"
+
+  let formEl = $(
+    "<form id = 'stock-look-up-form' class = 'uk-margin'></form>"
+  ).css({
+    "display": "flex",
+    "gap": "20px",
+    "flex-direction": "row",
+    "margin-top": "15px",
+    "margin-left": "20%",
+    "margin-right": "20%",
+    "padding": "20px",
+    "border-radius": "10px",
+    "background-color": "#090580",
+    "align-items": "center",
+    "color": "white",
+    "width": "fit-content"
+  });
+  //styling the header for the form
+  formEl.append($("<label for='ticker-input'>Search by Ticker</label>")).css({
+    "font-size": "20px",
+    //TODO choose font
+
+    //TODO SEARCH WILL BE TURNED INTO A FUNCTION.
+  });
+  //styling the
+  formEl.append(
+    $(
+      "<textarea id='ticker-input' name='ticker-input' rows='1' cols='10'></textarea>"
     ).css({
-      "display": "flex",
-      "gap": "20px",
-      "flex-direction": "row",
-      "margin-top": "15px",
-      "margin-left": "20%",
-      "margin-right": "20%",
-      "padding": "20px",
-      "border-radius": "10px",
-      "background-color": "#090580",
-      "align-items": "center",
-      "color": "white",
-      "width": "fit-content"
-    });
-    //styling the header for the form
-    formEl.append($("<label for='ticker-input'>Search by Ticker</label>")).css({
-      "font-size": "20px",
-      //TODO choose font
-  
-      //TODO SEARCH WILL BE TURNED INTO A FUNCTION.
-    });
-    //styling the
-    formEl.append(
-      $(
-        "<textarea id='ticker-input' name='ticker-input' rows='1' cols='10'></textarea>"
-      ).css({
-        "outline": "none",
-        "border": "none",
-        "resize": "none",
-        "border-radius": "20px",
-        "text-align": "center",
-        "height": "20px", // Set a specific height
-        "line-height": "20px", // Set line-height equal to the height
-      })
-    );
-    formEl
-      .append($("<input type='submit' id='ticker-input-submit' value='Search'>"))
-      .css({
-        "outline": "none",
-        "border": "none",
-      });
-    let sectionEl = $("<section>").attr("id", "sectionElForCard");
-    sectionEl.css({
-      "display": "flex",
-      "justify-content": "center"
+      "outline": "none",
+      "border": "none",
+      "resize": "none",
+      "border-radius": "20px",
+      "text-align": "center",
+      "height": "20px", // Set a specific height
+      "line-height": "20px", // Set line-height equal to the height
     })
-    $("main").before(sectionEl);
-  
-    sectionEl.append(formEl);
+  );
+  formEl
+    .append($("<input type='submit' id='ticker-input-submit' value='Search'>"))
+    .css({
+      "outline": "none",
+      "border": "none",
+    });
+  let sectionEl = $("<section>").attr("id", "sectionElForCard");
+  sectionEl.css({
+    "display": "flex",
+    "justify-content": "center"
+  })
+  $("main").before(sectionEl);
 
-    $("#ticker-input").on("keydown", function (event) {
-      if (event.originalEvent.key == "Enter"){
-        event.preventDefault()
-        $("#ticker-input-submit").trigger('click')
-      }
+  sectionEl.append(formEl);
+
+  $("#ticker-input").on("keydown", function (event) {
+    if (event.originalEvent.key == "Enter") {
+      event.preventDefault()
+      $("#ticker-input-submit").trigger('click')
     }
-    )
-    let displayedTickers = [];
+  }
+  )
+  let displayedTickers = [];
 
-    $("#ticker-input-submit").on("click", function (event) {
-      event.preventDefault();
-      let searchedTicker = $("#ticker-input").val().toUpperCase();
-      $("#ticker-input").val("");
- 
-      if (displayedTickers.includes(searchedTicker)){
-        //TO DO: Change to modal
-        alert("You already have this ticker displayed")
-        return
+  $("#ticker-input-submit").on("click", function (event) {
+    event.preventDefault();
+    let searchedTicker = $("#ticker-input").val().toUpperCase();
+    $("#ticker-input").val("");
+
+    if (displayedTickers.includes(searchedTicker)) {
+      //TO DO: Change to modal
+      alert("You already have this ticker displayed")
+      return
+    }
+
+    if (activeTickers.tickerList.includes(searchedTicker)) {
+      displayedTickers.push(searchedTicker)
+      stockPreviousClose(searchedTicker);
+    } else {
+      //To do: CHange to modal
+      alert("This ticker doesn't exist/isn't currently active");
+    }
+  });
+})()
+
+//run stockPreviousClose(favorites array) to display onto favorites page
+function stockPreviousClose(ticker) {
+  let apiUrl =
+    "https://api.polygon.io/v2/aggs/ticker/" +
+    ticker +
+    "/prev?adjusted=true&" +
+    "apiKey=" +
+    apiKey; //concatonates the endpoint
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.status !== 200) {
+        //To do: display modal clarifying the error
+        return new Promise();
       }
+      return response.json();
+    })
+    .then(function (data) {
+      if (data) {
+        // To do: add Style to this section element and their children
+        let sectionEl = $("<section>").attr("id", "card");
+        let headerEl = $("<h3>" + data.results[0].T + "</h3>"); //creates a header element with text content of the ticker Title
+        sectionEl.append(headerEl);
 
-      if (activeTickers.tickerList.includes(searchedTicker)) {
-        displayedTickers.push(searchedTicker)
-        stockPreviousClose(searchedTicker);
-      } else {
-        //To do: CHange to modal
-        alert("This ticker doesn't exist/isn't currently active");
+        //! adding favorite button
+        let favoriteButton = $("<button>")
+          .attr("class", "favorite-button")
+          .css({
+            "height": "fit-content",
+            "width": "fit-content",
+            "margin": "auto",
+          })
+          .text("Favorite");
+        //! appending favorite button
+        sectionEl.append(favoriteButton);
+
+        let ulEl = $("<ul>").css({
+          "list-style": "none",
+          padding: "10px",
+          margin: "0",
+        });
+        let openPriceLiEl = $("<li>Open Price: " + data.results[0].o + "</li>");
+        ulEl.append(openPriceLiEl);
+        let closePriceLiEl = $(
+          "<li>Close Price: " + data.results[0].c + "</li>"
+        );
+        ulEl.append(closePriceLiEl);
+        let highestPriceLiEl = $(
+          "<li>Highest Price: " + data.results[0].h + "</li>"
+        );
+        ulEl.append(highestPriceLiEl);
+        let lowestPriceLiEl = $(
+          "<li>Lowest Price: " + data.results[0].l + "</li>"
+        );
+        ulEl.append(lowestPriceLiEl);
+        let numOfTransactionsLiEl = $(
+          "<li>Number of transactions: " + data.results[0].n + "</li>"
+        );
+        ulEl.append(numOfTransactionsLiEl);
+        let tradingVolumeLiEl = $(
+          "<li>Trading Volume: " + data.results[0].v + "</li>"
+        );
+        ulEl.append(tradingVolumeLiEl);
+        let volumeWeightedAvgPrice = $(
+          "<li>Volume Weighted Average Price: " + data.results[0].vw + "</li>"
+        );
+        // $("main").css({
+        //   "display": "grid",
+        //   "grid-template-columns": "repeat(auto-fill, minmax(400px, 1fr))", // This will create as many columns as can fit without any of them having a width less than 250px
+        //   "gap": "20px",
+        // });
+
+        ulEl.append(volumeWeightedAvgPrice);
+
+        headerEl.css({
+          color: "white",
+        });
+        sectionEl.css({
+          "display": "flex",
+          "color": "white",
+          "background-color": "#080097",
+          "flex-direction": "column",
+          "width": "fit-content",
+          "text-align": "center",
+          // "gap": "5px",
+          "border-radius": "10px",
+          "padding": "15px",
+          "margin": "45px",
+        });
+        sectionEl.append(ulEl);
+        let divEl = $("<div>");
+        divEl.append(sectionEl)
+        $("main").append(divEl);
       }
     });
-  })()
-  
-  //run stockPreviousClose(favorites array) to display onto favorites page
-  function stockPreviousClose(ticker) {
-    let apiUrl =
-      "https://api.polygon.io/v2/aggs/ticker/" +
-      ticker +
-      "/prev?adjusted=true&" +
-      "apiKey=" +
-      apiKey; //concatonates the endpoint
-  
-    fetch(apiUrl)
-      .then(function (response) {
-        if (response.status !== 200) {
-          //To do: display modal clarifying the error
-          return new Promise();
-        }
-        return response.json();
-      })
-      .then(function (data) {
-        if (data) {
-          // To do: add Style to this section element and their children
-          let sectionEl = $("<section>").attr("id", "card");
-          let headerEl = $("<h3>" + data.results[0].T + "</h3>"); //creates a header element with text content of the ticker Title
-          sectionEl.append(headerEl);
-  
-          //! adding favorite button
-          let favoriteButton = $("<button>")
-            .attr("class", "favorite-button")
-            .css({
-              "height": "fit-content",
-              "width": "fit-content",
-              "margin": "auto",
-            })
-            .text("Favorite");
-          //! appending favorite button
-          sectionEl.append(favoriteButton);
-  
-          let ulEl = $("<ul>").css({
-            "list-style": "none",
-            padding: "10px",
-            margin: "0",
-          });
-          let openPriceLiEl = $("<li>Open Price: " + data.results[0].o + "</li>");
-          ulEl.append(openPriceLiEl);
-          let closePriceLiEl = $(
-            "<li>Close Price: " + data.results[0].c + "</li>"
-          );
-          ulEl.append(closePriceLiEl);
-          let highestPriceLiEl = $(
-            "<li>Highest Price: " + data.results[0].h + "</li>"
-          );
-          ulEl.append(highestPriceLiEl);
-          let lowestPriceLiEl = $(
-            "<li>Lowest Price: " + data.results[0].l + "</li>"
-          );
-          ulEl.append(lowestPriceLiEl);
-          let numOfTransactionsLiEl = $(
-            "<li>Number of transactions: " + data.results[0].n + "</li>"
-          );
-          ulEl.append(numOfTransactionsLiEl);
-          let tradingVolumeLiEl = $(
-            "<li>Trading Volume: " + data.results[0].v + "</li>"
-          );
-          ulEl.append(tradingVolumeLiEl);
-          let volumeWeightedAvgPrice = $(
-            "<li>Volume Weighted Average Price: " + data.results[0].vw + "</li>"
-          );
-          $("main").css({
-            "display": "grid",
-            "grid-template-columns": "repeat(auto-fill, minmax(400px, 1fr))", // This will create as many columns as can fit without any of them having a width less than 250px
-            "gap": "20px",
-          });
-  
-          ulEl.append(volumeWeightedAvgPrice);
-  
-          headerEl.css({
-            color: "white",
-          });
-          sectionEl.css({
-            display: "flex",
-            color: "white",
-            "background-color": "#080097",
-            "flex-direction": "column",
-            "width": "fit-content",
-            "text-align": "center",
-            // "gap": "5px",
-            "border-radius": "10px",
-            "padding": "15px",
-            "margin": "45px",
-          });
-          sectionEl.append(ulEl);
-  
-          $("main").prepend(sectionEl);
-        }
-      });
-  }
+}
