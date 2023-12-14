c75467ce30fa286d30e9f47458b5ecf91fe4316c
76500833 <145300587+76500833@users.noreply.github.com>
2023-12-14 17:51:00 -0500

    search button style

--- assets/js/search.js
+++ assets/js/search.js
@@ -64,22 +64,21 @@ let displayedTickers = [];
 
 //Displays the stock lookup functionality, and adds event listener to the submit to perform the utility.
 (function () {
-
   let formEl = $(
     "<form id = 'stock-look-up-form' class = 'uk-margin'></form>"
   ).css({
-    "display": "flex",
-    "gap": "20px",
+    display: "flex",
+    gap: "20px",
     "flex-direction": "row",
     "margin-top": "15px",
     "margin-left": "20%",
     "margin-right": "20%",
-    "padding": "20px",
+    padding: "20px",
     "border-radius": "10px",
     "background-color": "rgb(4, 0, 156)",
     "align-items": "center",
-    "color": "white",
-    "width": "fit-content"
+    color: "white",
+    width: "fit-content",
   });
   //styling the header for the form
   formEl.append($("<label for='ticker-input'>Search by Ticker</label>")).css({
@@ -93,39 +92,35 @@ let displayedTickers = [];
     $(
       "<textarea id='ticker-input' name='ticker-input' rows='1' cols='10'></textarea>"
     ).css({
-      "outline": "none",
-      "border": "none",
-      "resize": "none",
+      outline: "none",
+      border: "none",
+      resize: "none",
       "border-radius": "5px",
       "text-align": "center",
-      "height": "20px", // Set a specific height
+      height: "20px", // Set a specific height
       "line-height": "20px", // Set line-height equal to the height
     })
   );
-  formEl
-    .append($("<input type='submit' id='ticker-input-submit' value='Search'>"))
-    .css({
-      "outline": "none",
-      "border": "none",
-    });
+  formEl.append(
+    $("<input type='submit' id='ticker-input-submit' value='Search'>")
+  );
+
   let sectionEl = $("<section>").attr("id", "sectionElForCard");
   sectionEl.css({
-    "display": "flex",
+    display: "flex",
     "justify-content": "center",
-
-    
-  })
+    // text align
+  });
   $("main").before(sectionEl);
 
   sectionEl.append(formEl);
 
   $("#ticker-input").on("keydown", function (event) {
     if (event.originalEvent.key == "Enter") {
-      event.preventDefault()
-      $("#ticker-input-submit").trigger('click')
+      event.preventDefault();
+      $("#ticker-input-submit").trigger("click");
     }
-  }
-  )
+  });
 
   $("#ticker-input-submit").on("click", function (event) {
     event.preventDefault();
@@ -134,19 +129,19 @@ let displayedTickers = [];
 
     if (displayedTickers.includes(searchedTicker)) {
       //TO DO: Change to modal
-      alert("You already have this ticker displayed")
-      return
+      alert("You already have this ticker displayed");
+      return;
     }
 
     if (activeTickers.tickerList.includes(searchedTicker)) {
-      displayedTickers.push(searchedTicker)
+      displayedTickers.push(searchedTicker);
       stockPreviousClose(searchedTicker);
     } else {
       //To do: CHange to modal
       alert("This ticker doesn't exist/isn't currently active");
     }
   });
-})()
+})();
 
 //run stockPreviousClose(favorites array) to display onto favorites page
 function stockPreviousClose(ticker) {
@@ -156,8 +151,8 @@ function stockPreviousClose(ticker) {
     "/prev?adjusted=true&" +
     "apiKey=" +
     apiKey; //concatonates the endpoint
-    
-    fetch(apiUrl)
+
+  fetch(apiUrl)
     .then(function (response) {
       if (response.status !== 200) {
         //To do: display modal clarifying the error
@@ -169,171 +164,173 @@ function stockPreviousClose(ticker) {
       if (data) {
         let tickerSymbol = data.results[0].T;
         let divEl = $("<div>").attr("id", tickerSymbol);
-        let sectionEl = $("<section>").attr("id", "card")
-          .css({
-            "display": "flex",
-            "justify-content": "center",
-            "background-color": "rgb(4, 0, 81)"
-            
-            
-          })
-      
-        // let nameEl = $("<h3>") + nameForCard + "</h3>");
+        let sectionEl = $("<section>").attr("id", "card").css({
+          display: "flex",
+          "justify-content": "center",
+          "background-color": "rgb(4, 0, 81)",
+          "text-align": "center",
+        });
+
         let headerEl = $("<h3>" + tickerSymbol + "</h3>"); //creates a header element with text content of the ticker Title
         sectionEl.append(headerEl);
-        
+
         //adding close btn
-        let closeBtn = $("<button " + "class = 'uk-position-absolute " +
-        "uk-position-small " +
-        "uk-position-top-right' " +
-        "type='button' uk-close></button>");
-        
-        
-        sectionEl.append(closeBtn)
+        let closeBtn = $(
+          "<button " +
+            "class = 'uk-position-absolute " +
+            "uk-position-small " +
+            "uk-position-top-right' " +
+            "type='button' uk-close></button>"
+        );
 
+        sectionEl.append(closeBtn);
 
-     
         //TODO turn into modal
-          let newsModalButton = $("<button>")
-          .attr("class", "uk-button uk-button-default uk-margin-small-right")
+        let newsModalButton = $("<button>")
+          .attr(
+            "class",
+            "uk-button uk-button-default uk-margin-small-right uk-border-rounded"
+          )
           .attr("id", "newsModalButton")
           // .attr("class", "uk-button uk-button-default uk-margin-small-right uk-align-center")
           .attr("type", "button")
-          .attr("uk-toggle" ,"target: #newsModal")
-          .css({ 
-            "height": "fit-content",
-            //worked perfectly thanks kev
-            "width": "100%",
-            "margin": "auto",
-            "color": "white"
-          })
-          .text("news articles");
-          //! appending news button
-          sectionEl.append(newsModalButton);
-          let modal = $("<div>")
+          .attr("uk-toggle", "target: #newsModal")
           .css({
-            "width": "100%",
-            "background": "transperant"
+            height: "fit-content",
+            //worked perfectly thanks kev
+            width: "100%",
+            margin: "auto",
+            color: "white",
+          })
+          .text("Learn More");
+        //! appending news button
+        sectionEl.append(newsModalButton);
+        let modal = $("<div>")
+          .css({
+            width: "100%",
+            background: "transperant",
           })
           .attr("id", "newsModal")
           .attr("uk-modal", "uk-modal-dialog uk-margin-auto-vertical")
           // .css({
           //   "display": "flex",
-          //   "justify-content": "center"    
+          //   "justify-content": "center"
           //     })    // .attr("class", "uk-flex uk-flex-center uk-flex-middle uk-position-center uk-margin-auto-vertical")
           .append(
             $("<div>")
               .attr("class", "uk-modal-dialog uk-modal-body")
-              
-              
+
               .append(
                 $("<h2>").attr("class", "uk-modal-title"),
                 $("<p>").text("Modal content...").attr("class", "description"),
-                $("<p>").attr("class", "uk-text-right").append(
-                  $("<button>")
-                    .attr("class", "uk-button uk-button-default uk-modal-close")
-                    .attr("type", "button")
-                    .text("Cancel"),
-                  $("<button>")
-                    .attr("class", "uk-button uk-button-primary")
-                    .attr("type", "button")
-                    .text("Save")
-                )
+                $("<p>")
+                  .attr("class", "uk-text-right")
+                  .append(
+                    $("<button>")
+                      .attr(
+                        "class",
+                        "uk-button uk-button-default uk-modal-close"
+                      )
+                      .attr("type", "button")
+                      .text("Cancel"),
+                    $("<button>")
+                      .attr("class", "uk-button uk-button-primary")
+                      .attr("type", "button")
+                      .text("Save")
+                  )
               )
           );
 
-$("main").append(modal);
+        $("main").append(modal);
 
-          let ulEl = $("<ul>").css({
-            "list-style": "none",
-            padding: "10px",
-            margin: "0",
-          });
-          let openPriceLiEl = $("<li>Open Price: " + data.results[0].o + "</li>");
-          ulEl.append(openPriceLiEl);
-          let closePriceLiEl = $(
-            "<li>Close Price: " + data.results[0].c + "</li>"
-            );
-            ulEl.append(closePriceLiEl);
-            let highestPriceLiEl = $(
-              "<li>Highest Price: " + data.results[0].h + "</li>"
-              );
-              ulEl.append(highestPriceLiEl);
-              let lowestPriceLiEl = $(
-                "<li>Lowest Price: " + data.results[0].l + "</li>"
-                );
-                ulEl.append(lowestPriceLiEl);
-                let numOfTransactionsLiEl = $(
-                  "<li>Number of transactions: " + data.results[0].n + "</li>"
-                  );
-                  ulEl.append(numOfTransactionsLiEl);
-                  let tradingVolumeLiEl = $(
-                    "<li>Trading Volume: " + data.results[0].v + "</li>"
-                    );
-                    ulEl.append(tradingVolumeLiEl);
-                    let volumeWeightedAvgPrice = $(
-                      "<li>Volume Weighted Average Price: " + data.results[0].vw + "</li>"
-                      );
-                      // $("main").css({
-                        //   "display": "grid",
+        let ulEl = $("<ul>").css({
+          "list-style": "none",
+          padding: "10px",
+          margin: "0",
+        });
+        let openPriceLiEl = $("<li>Open Price: " + data.results[0].o + "</li>");
+        ulEl.append(openPriceLiEl);
+        let closePriceLiEl = $(
+          "<li>Close Price: " + data.results[0].c + "</li>"
+        );
+        ulEl.append(closePriceLiEl);
+        let highestPriceLiEl = $(
+          "<li>Highest Price: " + data.results[0].h + "</li>"
+        );
+        ulEl.append(highestPriceLiEl);
+        let lowestPriceLiEl = $(
+          "<li>Lowest Price: " + data.results[0].l + "</li>"
+        );
+        ulEl.append(lowestPriceLiEl);
+        let numOfTransactionsLiEl = $(
+          "<li>Number of transactions: " + data.results[0].n + "</li>"
+        );
+        ulEl.append(numOfTransactionsLiEl);
+        let tradingVolumeLiEl = $(
+          "<li>Trading Volume: " + data.results[0].v + "</li>"
+        );
+        ulEl.append(tradingVolumeLiEl);
+        let volumeWeightedAvgPrice = $(
+          "<li>Volume Weighted Average Price: " + data.results[0].vw + "</li>"
+        );
+        // $("main").css({
+        //   "display": "grid",
         //   "grid-template-columns": "repeat(auto-fill, minmax(400px, 1fr))", // This will create as many columns as can fit without any of them having a width less than 250px
         //   "gap": "20px",
         // });
-        
+
         ulEl.append(volumeWeightedAvgPrice);
-        
+
         headerEl.css({
           color: "white",
         });
         sectionEl.css({
-          "display": "flex",
-          "color": "white",
+          display: "flex",
+          color: "white",
           "background-color": "#080097",
           "flex-direction": "column",
-          "width": "fit-content",
+          width: "fit-content",
+          //text align left except ticker (in a span)
           "text-align": "center",
-          "position": "relative",
+          position: "relative",
           "border-radius": "10px",
-          "padding": "15px",
-          "margin": "45px",
+          padding: "15px",
+          margin: "45px",
         });
         sectionEl.append(ulEl);
         divEl.append(sectionEl);
         $("main").append(divEl);
         //makes the close button erase the div that contains it, and removes it from the displayed tickers array
-        closeBtn.attr("id",tickerSymbol + "-btn")
-        $("#" + tickerSymbol + "-btn").on("click",function (){
-          $("#" + tickerSymbol).remove()
-         let index = displayedTickers.indexOf(tickerSymbol)
-         displayedTickers.splice(index, 1)
-        })
+        closeBtn.attr("id", tickerSymbol + "-btn");
+        $("#" + tickerSymbol + "-btn").on("click", function () {
+          $("#" + tickerSymbol).remove();
+          let index = displayedTickers.indexOf(tickerSymbol);
+          displayedTickers.splice(index, 1);
+        });
       }
     });
-  }
+}
 
+$(document).on("click", "#card #newsModalButton", function () {
+  //grabs the ticker of the card so it can be plugged into the endpoint
+  let ticker = $(this).siblings("h3").text();
 
-    $(document).on('click', "#card #newsModalButton", (function() {
-
-      //grabs the ticker of the card so it can be plugged into the endpoint
-      let ticker = $(this).siblings("h3").text();
-
-      let alphaVantageKey = "PUZOI2F17H6KBPQC"
-      let apiUrl = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey= +${alphaVantageKey}`;
-      fetch(apiUrl)
-          .then(function (response) {
-              if (response.status !== 200) {
-                  throw new Error("Not 200 response");
-              }
-              return response.json();
-          })
-          .then(function (data) {
-          
-              $(".uk-modal-title").text(data.Name)
-              $(".description").text(data.Description)
-              
-          })
-          .catch(function (error) {
-              console.log('Error:', error);
-          });
-  }));
-
+  let alphaVantageKey = "PUZOI2F17H6KBPQC";
+  let apiUrl = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey= +${alphaVantageKey}`;
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.status !== 200) {
+        throw new Error("Not 200 response");
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(ticker);
+      $(".uk-modal-title").text(data.Name);
+      $(".description").text(data.Description);
+    })
+    .catch(function (error) {
+      console.log("Error:", error);
+    });
+});
